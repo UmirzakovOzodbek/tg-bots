3ae87b95048ff1891849ab171a5cca61367825a1
UmirzakovOzodbek <122611764+UmirzakovOzodbek@users.noreply.github.com>
2023-02-15 20:28:57 +0500

    Update utils.py

--- register_bot/utils.py
+++ register_bot/utils.py
@@ -4,26 +4,25 @@ import os
 from api.weather import WeatherManager
 
 
-def write_row_to_csv(file_path, header, row):
-    with open(file_path, "a+", newline="\n") as f:
-        csv_writer = csv.DictWriter(f, header)
+def write_to_csv(file_path, header, row):
+    with open(file_path, "a", newline="\n", encoding="utf8") as file:
+        csv_writer = csv.DictWriter(file, header)
         if os.path.getsize(file_path) == 0:
             csv_writer.writeheader()
-        # csv_writer.writerow(student.get_attrs_for_csv_writer())
         csv_writer.writerow(row)
     print("Row add successfully.")
 
 
 def get_language_code_by_chat_id(chat_id, file_path):
-    with open(file_path) as f:
+    with open(file_path, encoding="utf8") as f:
         csv_reader = csv.DictReader(f)
-        langs = [
+        language = [
             row.get("language_code")
             for row in csv_reader
             if int(row.get("id")) == chat_id
         ]
-        if langs:
-            return langs[-1]
+        if language:
+            return language[-1]
         else:
             return "uz"
 
@@ -34,10 +33,10 @@ def is_exist_chat_id(chat_id):
         return chat_id in [int(row.get("chat_id")) for row in csv_reader]
 
 
+def get_fullname(first_name, last_name):
+    return f"{first_name} {last_name}" if last_name else first_name
+
+
 def get_weather_days():
     temperatures = WeatherManager().get_daily_temperature()
     return [day_temp.get("day") for day_temp in temperatures]
-
-
-def get_fullname(first_name, last_name):
-    return f"{first_name} {last_name}" if last_name else first_name
